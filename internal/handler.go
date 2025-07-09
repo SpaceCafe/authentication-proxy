009a6134a5bb1a3ec874b8f958674a439122ab5e
Lars Thoms <lars@thoms.io>
2025-07-09 10:56:47 +0200

    ♻️ refactor handleProxy function for better request handling

--- internal/handler.go
+++ internal/handler.go
@@ -1,15 +1,42 @@
 package internal
 
 import (
+	"net"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 
 	"github.com/gin-gonic/gin"
 )
 
-func handleProxy(upstreamURL *url.URL) gin.HandlerFunc {
+func handleProxy(upstreamURL *url.URL, authHeader string) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		proxy := httputil.NewSingleHostReverseProxy(upstreamURL)
+
+		originalDirector := proxy.Director
+		proxy.Director = func(req *http.Request) {
+			originalDirector(req)
+
+			// Remove potentially sensitive headers
+			req.Header.Del("Authorization")
+			req.Header.Del(authHeader)
+
+			// Add proxy information
+			if req.Header.Get("X-Forwarded-Host") == "" {
+				req.Header.Set("X-Forwarded-Host", req.Host)
+			}
+			if req.Header.Get("X-Forwarded-Proto") == "" {
+				req.Header.Set("X-Forwarded-Proto", req.Proto)
+			}
+			if clientIP, _, err := net.SplitHostPort(ctx.Request.RemoteAddr); err == nil {
+				if forwardedIP := req.Header.Get("X-Forwarded-For"); forwardedIP == "" {
+					req.Header.Set("X-Forwarded-For", clientIP)
+				} else {
+					req.Header.Set("X-Forwarded-For", forwardedIP+", "+clientIP)
+				}
+			}
+		}
+
 		proxy.ServeHTTP(ctx.Writer, ctx.Request)
 	}
 }
